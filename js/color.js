614d88b3dc08b54c1ca87aba33c9ac6532de5122
NaHoJin <ghwls3995@gmail.com>
2024-04-12 15:22:54 +0900

    4.12 6교시 jquery 변환 확인

--- js/color.js
+++ js/color.js
@@ -1,12 +1,22 @@
-var Body = {
-    setColor: function (color) {
-      document.querySelector('body').style.color = color;
-    },
-    setBackgroundColor: function (color) {
-      document.querySelector('body').style.backgroundColor = color;
-    }
-  }
+// var Body = {
+//     setColor: function (color) {
+//       document.querySelector('body').style.color = color;
+//     },
+//     setBackgroundColor: function (color) {
+//       document.querySelector('body').style.backgroundColor = color;
+//     }
+//   }
 
+//jQuerry 버전 , 도구이용
+  var Body = {
+    setColor: function(color) {
+   $('body').css('color', color);
+    },
+    setBackgroundColor: function(color) {
+    $('body').css('backgroundColor', color);
+    }
+   }
+// 생 자바스크립트 버전 , 바닐라 자바스크립트 ver 
   var Links = {
     setColor: function (color) {
       var alist = document.querySelectorAll('a');
@@ -16,7 +26,12 @@ var Body = {
         i = i + 1;
       }
     }
-  }  
+  } 
+  
+  // // jQuery버전
+  // function setColor(color){
+  //   $('a').css('color', color);
+  // }
 
   function nightDayHandler(self) {
     if (self.value === 'night') {
